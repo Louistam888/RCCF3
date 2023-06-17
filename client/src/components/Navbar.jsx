f419e1e4a8fec9be675c970d9d5fcc8e11577dbd
Louistam888 <louistam888@gmail.com>
2023-06-17 14:21:24 -0400

    setting up navbar framework

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import {
   Box,
   Flex,
   HStack,
+  VStack,
+  Heading,
   Link,
   IconButton,
   Icon,
@@ -10,12 +12,42 @@ import {
   Button,
   Stack,
   useColorModeValue,
+  useColorMode,
+  Image,
 } from "@chakra-ui/react";
 import { Link as ReactLink } from "react-router-dom";
-import {HamburgerIcon}
+import { HamburgerIcon, CloseIcon, MoonIcon, sunIcon } from "@chakra-ui/icons";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <Box bg={useColorModeValue("whiteAlpha.900", "blackAlpha.900")} px={10}>
+      <Flex h={90} alignItems="center" justifyContent="space-between" border="2px">
+        <HStack>
+          <Link as={ReactLink} to="/">
+            <Box display="flex">
+              <Flex alignItems="center">
+                <Image src="assets/RCCF.png" h="80px" w="80px" />
+              </Flex>
+              <h1>
+                <span className="h1Red">ROYAL CANADIAN</span>
+                <span className="h1Blue">CHAIR FORCE</span>
+              </h1>
+            </Box>
+          </Link>
+        </HStack>
+
+        <IconButton
+          size="md"
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          display={{ md: "none" }}
+          onClick={isOpen ? onClose : onOpen}
+        />
+      </Flex>
+    </Box>
+  );
 };
 
 export default Navbar;
