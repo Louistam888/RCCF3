15c73a7dc2a2f5ac48c98eaef7c4daaf8583c055
Louistam888 <louistam888@gmail.com>
2023-06-17 12:39:59 -0400

    started navBar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import {
+  Box,
+  Flex,
+  HStack,
+  Link,
+  IconButton,
+  Icon,
+  Text,
+  useDisclosure,
+  Button,
+  Stack,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { Link as ReactLink } from "react-router-dom";
+import {HamburgerIcon}
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
