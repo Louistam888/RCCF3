f930cd5bc8732d940efe1ffcb9d43dac3dfc3107
Louistam888 <louistam888@gmail.com>
2023-06-17 15:25:04 -0400

    completed setting up logo and site name in navbar with breakpoints

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
+// Supports weights 100-800
+import '@fontsource/anek-latin/300.css';
+import "@fontsource/roboto";
+
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import theme from "./theme/Theme";
+
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Router>
         <Navbar />
         <main>{/* <Routes></Routes> */}</main>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -23,18 +23,27 @@ const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Box bg={useColorModeValue("whiteAlpha.900", "blackAlpha.900")} px={10}>
-      <Flex h={90} alignItems="center" justifyContent="space-between" border="2px">
+    <Box bg={useColorModeValue("whiteAlpha.900", "blackAlpha.900")} px={10} border="1px">
+      <Flex h={90} alignItems="center" justifyContent="space-between">
         <HStack>
           <Link as={ReactLink} to="/">
             <Box display="flex">
               <Flex alignItems="center">
-                <Image src="assets/RCCF.png" h="80px" w="80px" />
+                {/* <Image src="assets/RCCF.png" h="80px" w="80px" /> */}
+                <Image src="assets/RCCF.png" boxSize={{ base: "50px", sm: "60px", md: "70px", lg: "80px" }} />
               </Flex>
-              <h1>
-                <span className="h1Red">ROYAL CANADIAN</span>
-                <span className="h1Blue">CHAIR FORCE</span>
-              </h1>
+              <Flex alignItems="center" pl={{ base: "5px", md: "10px" }}>
+                <Heading
+                  as="h1"
+                  display="flex"
+                  flexDirection="column"
+                  fontSize={{ base: "1.05rem", sm: "1.3rem", md: "1.4rem", lg: "1.8rem" }}
+                  lineHeight="1"
+                >
+                  <span className="h1Red">ROYAL CANADIAN</span>
+                  <span className="h1Blue">CHAIR FORCE</span>
+                </Heading>
+              </Flex>
             </Box>
           </Link>
         </HStack>
