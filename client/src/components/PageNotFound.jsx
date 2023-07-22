f2e40d067f6c1b1d3fecce02720994a5c6195c71
Louistam888 <louistam888@gmail.com>
2023-07-22 13:26:11 -0400

    completed 404 error screen desktop and responsive

--- client/src/components/PageNotFound.jsx
+++ client/src/components/PageNotFound.jsx
@@ -1,22 +1,28 @@
-import { Box, Flex, Image, Heading, Alert, AlertIcon, AlertTitle, AlertDescription } from "@chakra-ui/react";
+import { Box, Flex, Image, Heading, Alert, AlertIcon, AlertDescription, AlertTitle, Text } from "@chakra-ui/react";
 
 const PageNotFound = ({ error }) => {
   return (
-    <Box w="100%" position="relative">
-      <Alert status="error"  h="100px" justifyContent="center" display="flex" flexDirection="column">
-        <AlertIcon />
-        <AlertTitle>Sorry, this page cannot be found.</AlertTitle>
-        <AlertDescription>{error}</AlertDescription>
-      </Alert>
-      <Image
-        src={`${process.env.PUBLIC_URL}/assets/404.jpg`}
-        alt="#"
-        w="100%"
-        h="500px"
-        objectFit="cover"
-        objectPosition="70% center"
-        display={{ base: "none", sm: "block" }}
-      />
+    <Box position="relative">
+      <Box w="100%">
+        <Alert status="error" justifyContent="center" display="flex" flexDirection="column">
+          <Flex alignItems="center" fontSize={{base:"sm", sm:"lg"}}>
+            <AlertIcon my="10px" />
+            <AlertTitle>{error}</AlertTitle>
+          </Flex>
+          <AlertDescription fontSize={{ base: "2xl", sm: "3xl" }} my="10px" lineHeight="30px "textAlign="center">
+            Sorry, this page cannot be found.
+          </AlertDescription>
+        </Alert>
+        <Image
+          src={`${process.env.PUBLIC_URL}/assets/404.jpg`}
+          alt="#"
+          w="100%"
+          h="500px"
+          objectFit="cover"
+          objectPosition="70% center"
+          // display={{ base: "none", sm: "block" }}
+        />
+      </Box>
     </Box>
   );
 };
