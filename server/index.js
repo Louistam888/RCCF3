1ef8559046abc3068cbac985e19cc64053e731d0
Louistam888 <louistam888@gmail.com>
2023-08-31 20:37:49 -0400

    completed userRoutes

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import path from "path";
 
 //ROUTES
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 connectToDatabase();
@@ -15,15 +16,14 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 app.use("/api/products", productRoutes);
-
 app.use("*", (req, res) => {
   res.status(404).send("Not Found");
 });
-
 app.use("/*", (req, res) => {
   res.status(404).send("Not Found");
 });
-
 app.listen(port, () => {
   `Server runs on port ${port}.`;
 });
+
+app.use("/api/users", userRoutes);
