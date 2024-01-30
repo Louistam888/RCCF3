5d378c5141e825dcf189fa478c9ce450d08b8af6
Louistam888 <louistam888@gmail.com>
2024-01-30 17:12:22 -0500

    rearranged code in paypalbutton

--- client/src/components/PayPalButton.jsx
+++ client/src/components/PayPalButton.jsx
@@ -3,11 +3,10 @@ import {
   PayPalButtons,
   usePayPalScriptReducer,
 } from "@paypal/react-paypal-js";
-import{ useDisclosure } from "@chakra-ui/react"
+import{ Stack,Spinner } from "@chakra-ui/react"
 import PAYPAL_CLIENT_ID from "../client_id"
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-
 
 // This value is from the props in the UI
 const style = { layout: "vertical" };
@@ -88,77 +87,78 @@ const ButtonWrapper = ({
   );
 };
 
-const PayPalButton = ({ total, onPaymentSuccess, onPaymentError, disabled }) => {
-  const [paypalClient, setPayPalClient] = useState(null);
 
-  useEffect(() => {
-    const paypalkey = async () => {
-      const { data: clientId } = await axios.get('/api/config/paypal');
-      setPayPalClient(clientId);
-    };
-    paypalkey();
-  }, [paypalClient]);
-  return !paypalClient ? (
-    <Stack direction='row' spacing={4} alignSelf='center'>
-      <Spinner mt={20} thickness='2px' speed='0.65s' emptyColor='gray.200' color='orange.500' size='xl' />
-    </Stack>
-  ) : (
-    <PayPalScriptProvider
-      options={{
-        'client-id': paypalClient,
-      }}>
-      <PayPalButtons
-        disabled={disabled}
-        forceReRender={[total(), paypalClient]}
-        createOrder={(data, actions) => {
-          return actions.order.create({
-            purchase_units: [
-              {
-                amount: {
-                  value: total(),
-                },
-              },
-            ],
-          });
-        }}
-        onApprove={(data, actions) => {
-          return actions.order.capture().then((details) => {
-            onPaymentSuccess(data);
-          });
-        }}
-        onError={(err) => {
-          onPaymentError();
-        }}
-      />
-    </PayPalScriptProvider>
+const PayPalButton = ({
+  total,
+  onPaymentSuccess,
+  onPaymentError,
+  buttonDisabled,
+}) => {
+  const disabledStatus = buttonDisabled;
+  
+  return (
+    <div style={{ maxWidth: "750px", minHeight: "200px" }}>
+      <PayPalScriptProvider
+        options={{ clientId: PAYPAL_CLIENT_ID, components: "buttons", currency: "USD" }}
+      >
+        <ButtonWrapper
+          showSpinner={false}
+          disabledStatus={disabledStatus}
+          onPaymentError={onPaymentError}
+          onPaymentSuccess={onPaymentSuccess}
+          total={total}
+        />
+      </PayPalScriptProvider>
+    </div>
   );
 };
 
-export default PayPalButton;
+// const PayPalButton = ({ total, onPaymentSuccess, onPaymentError, disabled }) => {
+//   const [paypalClient, setPayPalClient] = useState(null);
 
-// const PayPalButton = ({
-//   total,
-//   onPaymentSuccess,
-//   onPaymentError,
-//   buttonDisabled,
-// }) => {
-//   const disabledStatus = buttonDisabled;
-  
-//   return (
-//     <div style={{ maxWidth: "750px", minHeight: "200px" }}>
-//       <PayPalScriptProvider
-//         options={{ clientId: PAYPAL_CLIENT_ID, components: "buttons", currency: "USD" }}
-//       >
-//         <ButtonWrapper
-//           showSpinner={false}
-//           disabledStatus={disabledStatus}
-//           onPaymentError={onPaymentError}
-//           onPaymentSuccess={onPaymentSuccess}
-//           total={total}
-//         />
-//       </PayPalScriptProvider>
-//     </div>
+//   useEffect(() => {
+//     const paypalkey = async () => {
+//       const { data: clientId } = await axios.get('/api/config/paypal');
+//       setPayPalClient(clientId);
+//     };
+//     paypalkey();
+//   }, [paypalClient]);
+//   return !paypalClient ? (
+//     <Stack direction='row' spacing={4} alignSelf='center'>
+//       <Spinner mt={20} thickness='2px' speed='0.65s' emptyColor='gray.200' color='orange.500' size='xl' />
+//     </Stack>
+//   ) : (
+//     <PayPalScriptProvider
+//       options={{
+//         'client-id': paypalClient,
+//       }}>
+//       <PayPalButtons
+//         disabled={disabled}
+//         forceReRender={[total(), paypalClient]}
+//         createOrder={(data, actions) => {
+//           return actions.order.create({
+//             purchase_units: [
+//               {
+//                 amount: {
+//                   value: total(),
+//                 },
+//               },
+//             ],
+//           });
+//         }}
+//         onApprove={(data, actions) => {
+//           return actions.order.capture().then((details) => {
+//             onPaymentSuccess(data);
+//           });
+//         }}
+//         onError={(err) => {
+//           onPaymentError();
+//         }}
+//       />
+//     </PayPalScriptProvider>
 //   );
 // };
 
 // export default PayPalButton;
+
+export default PayPalButton;
