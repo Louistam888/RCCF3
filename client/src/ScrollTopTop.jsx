d4389be2cb7a3ad56c0db80771ac4912c6fe7638
Louistam888 <louistam888@gmail.com>
2024-03-02 17:02:25 -0500

    added scrolltotop

---
+++ client/src/ScrollTopTop.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const container = document.documentElement;
+
+    container.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [pathname]);
+
+  return null;
+};
+
+export default ScrollToTop;
