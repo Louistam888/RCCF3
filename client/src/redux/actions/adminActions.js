68740d2ad316dbc58a89bf15accab7a49e1f1922
Louistam888 <louistam888@gmail.com>
2024-08-05 13:47:08 -0400

    added console logs for debugging

--- client/src/redux/actions/adminActions.js
+++ client/src/redux/actions/adminActions.js
@@ -181,7 +181,6 @@ export const updateProduct =
           "Content-Type": "application/json",
         },
       };
-      console.log("config", config);
       const { data } = await axios.put(
         `api/products`,
         {
