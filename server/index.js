b08dff783d0786d5ea1d8077408838064b8dadc2
Louistam888 <louistam888@gmail.com>
2024-08-05 15:22:36 -0400

    added app use back

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ import stripeRoutes from "./routes/stripeRoutes.js";
 dotenv.config();
 connectToDatabase();
 
-// const app = express();
+const app = express();
 app.use(express.json());
 
 app.use(
